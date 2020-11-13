f70db3bdf86b40b34c8c91b027b352ac09f5d465
Giorgio Enrico Turro <turro.giorgio@gmail.com>
2020-11-13 19:43:05 +0100

    Added Stores

--- Angular/first-app/src/app/shop/shop.component.ts
+++ Angular/first-app/src/app/shop/shop.component.ts
@@ -16,8 +16,12 @@ selectedShop: Shop;
 
   ngOnInit() {
     this.shops=[
-      new Shop('Jumping', 'Via Roma 102', 'Torino'),
-      new Shop('Giorgi8', 'Via Giorgione 59', 'Torino')
+      new Shop('Jumping', 'Via Roma, 102', 'Torino'),
+      new Shop('Giorgi8', 'Via Giorgione, 59', 'Torino'),
+      new Shop('Paradiso Brugola', 'Piazza delle Armi, 3', 'Torino'),
+      new Shop('Shot', 'Via Gianfrancesco Bellezia, 15', 'Torino'),
+      new Shop('Birbe di Pollo Store', 'Via Dei Polli, 13', 'Torino'),
+      new Shop('Un Altro', 'Viale Altrove, 99', 'Torino')
     ];
     console.log(this.shops);
   }
