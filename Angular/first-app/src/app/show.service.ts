ce3ebefa7d127b9cb560f439658a4e0f9fabc93e
Paolo <despaolo37@gmail.com>
2020-12-03 22:33:41 +0100

    Fixed toggle shop menu

--- Angular/first-app/src/app/show.service.ts
+++ Angular/first-app/src/app/show.service.ts
@@ -5,8 +5,21 @@ import { Injectable } from '@angular/core';
 })
 export class ShowService {
 show: boolean = false;
+currentShop: string = "";
   constructor() { }
-  toggleMenuComponent(){
-    this.show=!this.show;
+  toggleMenuComponent(newShop: string){
+    if(newShop==this.currentShop){
+      this.currentShop=newShop;
+      this.show=!this.show;
+      console.log(this.show);
+    }if(this.currentShop==""){
+      this.show=!this.show;
+      this.currentShop=newShop;
+      console.log(this.show);
+    }if(newShop!==this.currentShop){
+      this.show=true;
+      this.currentShop=newShop;
+      console.log(this.show);
+    }
   }
 }
