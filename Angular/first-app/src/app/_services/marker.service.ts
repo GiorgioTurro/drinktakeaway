94137aa3a52a5cbe886b234bee9c0b08e62eb7fa
Giorgio Enrico Turro <turro.giorgio@gmail.com>
2020-12-08 16:22:52 +0100

    Banana in pigiama

--- Angular/first-app/src/app/_services/marker.service.ts
+++ Angular/first-app/src/app/_services/marker.service.ts
@@ -13,11 +13,19 @@ export class MarkerService {
   constructor(private http: HttpClient, private popupService: PopUpService) { }
 
   makeCapitalMarkers(map: L.map): void {
+    var beerIcon = L.icon({
+    iconUrl: '/assets/images/favicon.png',
+    iconSize:     [30, 30], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
     this.http.get(this.capitals).subscribe((res: any) => {
       for (const c of res.features) {
         const lat = c.geometry.coordinates[0];
         const lon = c.geometry.coordinates[1];
-        const marker = L.marker([lon, lat]);
+        const marker = L.marker([lon, lat], {icon: beerIcon});
         marker.bindPopup(this.popupService.makeCapitalPopup(c)).addTo(map);
       }
     });
